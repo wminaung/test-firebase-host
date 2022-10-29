231ff628eab9574c8a11f6b52e7846b84a64e12e
Win Min Aung <minwin243@gmail.com>
2022-10-30 00:14:29 +0630

    Project is Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Login from "./components/Login";
-import Contact from "./components/Contact";
-import { Provider } from "react-redux";
-import { store } from "./store";
 
 function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Router>
-      </Provider>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
